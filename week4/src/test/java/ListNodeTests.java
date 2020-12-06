8aef9bd364f942f1f088a19904927a3b72f32571
plzprayme <dbfpzk142@gmail.com>
2020-12-07 00:02:57 +0900

    INIT VAR FROM BeforeEach

--- week4/src/test/java/ListNodeTests.java
+++ week4/src/test/java/ListNodeTests.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -5,14 +7,19 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class ListNodeTests {
 
-    @Test
-    @DisplayName("GET LAST TAIL TEST")
-    void getLastTailTest() {
-        ListNode listNode = new ListNode(0,
+    ListNode listNode;
+
+    @BeforeEach
+    void init() {
+        this.listNode = new ListNode(0,
                 new ListNode(1,
                         new ListNode(2,
                                 new ListNode(4, null))));
+    }
 
+    @Test
+    @DisplayName("GET LAST TAIL TEST")
+    void getLastTailTest() {
         boolean isCorrect = listNode.getLastTail().getValue() == 4;
         assumeTrue(isCorrect);
     }
